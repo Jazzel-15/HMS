61a67e9bb6a1f142c6ee0518e1757fa09ec23a48
Jessica <jessalvares203@gmail.com>
2024-07-10 10:09:40 +0530

    Initial Message

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html',
+        contact: 'contact.html'
+      }
+    }
+  }
+});
